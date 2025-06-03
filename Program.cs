d57c85dea4e932b383a3430386a3e09f2a293f18
LAB53 <lab53@fatec.com>
2025-06-03 09:54:37 -0300

    Correção

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
 ﻿Console.Clear();
 
-Console.WriteLine("--- Carrinho de Compras - Você na Moda ---");
+Console.WriteLine("--- Carrinho de Compras Você na Moda ---");
 
 var produtosDisponiveis = new List<Produto>()
 {
     new Produto() { Codigo = 1, Descricao = "Calça Jeans TM52", Preco = 75 },
     new Produto() { Codigo = 2, Descricao = "Vestido Florido TM54", Preco = 115 },
     new Produto() { Codigo = 3, Descricao = "Colete Versage TM50", Preco = 99 },
-    new Produto() { Codigo = 4, Descricao = "Saia Longuete TM56", Preco = 105 },
-};
+    new Produto() { Codigo = 4, Descricao = "Saia Longuete TM56", Preco = 105 },};
 
 var carrinho = new CarrinhoDeCompras();
 
 string opcao = "";
 while (opcao != "F")
+
 {
     Console.Write("\n[P]rodutos, [A]dicionar, [V]isualizar, [L]impar, [F]inalizar: ");
     opcao = Console.ReadLine()!;
@@ -36,14 +36,12 @@ while (opcao != "F")
             if (produto == null)
             {
                 Console.WriteLine("Não encontrado.");
-                break;
-            }
+                break;}
 
             if (quantidade <= 0)
             {
                 Console.WriteLine("Quantidade inválida.");
-                break;
-            }
+                break;}
 
             carrinho.AdicionarItemAoCarrinho(produto, quantidade);
             break;
@@ -55,57 +53,46 @@ while (opcao != "F")
 
         case "L":
             carrinho.LimparCarrinho();
-            break;
-    }
-}
+            break;}}
 
 void ExibirListaDeProdutos(List<Produto> produtos)
 {
     foreach (var produto in produtos)
-        Console.WriteLine($"{produto.Codigo}: {produto.Descricao}, por {produto.Preco:C2}");
-}
+        Console.WriteLine($"{produto.Codigo}: {produto.Descricao}, por {produto.Preco:C2}");}
 
 void ExibirCarrinhoDeCompras(CarrinhoDeCompras carrinho)
 {
     foreach (var item in carrinho.Itens)
         Console.WriteLine($"{item.Quantidade} x {item.Produto.Descricao} = {(item.Produto.Preco * item.Quantidade):C2}");
-    Console.WriteLine($"\nTotal = {carrinho.Total():C2}");
-}
+    Console.WriteLine($"\nTotal = {carrinho.Total():C2}");}
 
 class Produto
 {
     public int Codigo { get; set; } = default!;
     public string Descricao { get; set; } = default!;
-    public decimal Preco { get; set; } = default!;
-}
+    public decimal Preco { get; set; } = default!;}
 
 class ItemDoCarrinho
 {
     public Produto Produto { get; set; } = default!;
-    public int Quantidade { get; set; }
-}
+    public int Quantidade { get; set; }}
 
 class CarrinhoDeCompras
 {
     public List<ItemDoCarrinho> Itens = new();
     public void LimparCarrinho()
     {
-        Itens.Clear();
-    }
+        Itens.Clear();}
     public void AdicionarItemAoCarrinho(Produto produto, int quantidade)
     {
         var noCarrinho = Itens.Find(i => i.Produto == produto);
         if (noCarrinho != null)
         {
-            noCarrinho.Quantidade += quantidade;
-        }
+            noCarrinho.Quantidade += quantidade;}
         else
         {
-            Itens.Add(new ItemDoCarrinho() { Produto = produto, Quantidade = quantidade });
-        }
-    }
+            Itens.Add(new ItemDoCarrinho() { Produto = produto, Quantidade = quantidade });}}
     public decimal Total()
+
     {
-        return Itens.Sum(item => item.Produto.Preco * item.Quantidade);
-    }
-}
+        return Itens.Sum(item => item.Produto.Preco * item.Quantidade);}}
